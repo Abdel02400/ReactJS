8c230a02c8bbd539a4c7bb4cac6617b0447833ad
Abdel <abdelmess02400@hotmail.fr>
2018-09-10 15:13:24 +0200

    troisieme modif

--- src/Formulaire.js
+++ src/Formulaire.js
@@ -46,6 +46,9 @@ const styles = theme => ({
   submit: {
     marginTop: theme.spacing.unit * 3,
   },
+  erreur: {
+    color: 'red',
+  }
 });
 
 const emailRegex = /^[a-z0-9._-]+@[a-z0-9._-]+\.[a-z]{2,3}$/;
@@ -53,8 +56,6 @@ const telRegex = /^(06|07)[0-9]{8}$/;
 const alphaOnlyRegex = /^[A-Za-z]+$/;
 const dateRegex = /(\d+)(-|\/)(\d+)(?:-|\/)(?:(\d+)\s+(\d+):(\d+)(?::(\d+))?(?:\.(\d+))?)?/;
 
-const erreurNom = "";
-console.log(erreurNom);
 class Formulaire extends React.Component {
 
   state = {
@@ -64,7 +65,11 @@ class Formulaire extends React.Component {
     email: "",
     tel: "",
     date: "",
-    validation: "",
+    erreurNom: "",
+    erreurPrenom: "",
+    erreurEmail: "",
+    erreurTel: "",
+    erreurDate: "",
   };
 
    handleChange = e => {
@@ -73,24 +78,54 @@ class Formulaire extends React.Component {
 
    Change = e => {
 
-    this.erreurNom = "";
-    this.setState({ [e.target.name]: e.target.value });
+    this.setState({ 
+      [e.target.name]: e.target.value,
+      erreurNom: "",
+      erreurPrenom: "",
+      erreurEmail: "",
+      erreurTel: "",
+      erreurDate: "",
+    });
   };
 
   onSubmit = e => {
 
-      if( (!alphaOnlyRegex.test(this.state.nom)) || (!alphaOnlyRegex.test(this.state.prenom)) || (!emailRegex.test(this.state.email)) || (!telRegex.test(this.state.tel)) || (!dateRegex.test(this.state.date))) {
+      if( (!alphaOnlyRegex.test(this.state.nom)) || (!alphaOnlyRegex.test(this.state.prenom)) || (!emailRegex.test(this.state.email)) || (!telRegex.test(this.state.tel)) || (!dateRegex.test(this.state.date)) ) {
         
           if( (!alphaOnlyRegex.test(this.state.nom)) ){
             e.preventDefault();
-            this.state.validation = "erreur";
-            this.props.onSubmit(this.state);
+            this.setState({ 
+              erreurNom: "le nom doit être uniquement composer de lettres",
+            });
           }
+          if( (!alphaOnlyRegex.test(this.state.prenom)) ){
+            e.preventDefault();
+            this.setState({ 
+              erreurPrenom: "le prénom doit être uniquement composer de lettres",
+            });
+          }
+          if( (!emailRegex.test(this.state.email)) ){
+            e.preventDefault();
+            this.setState({ 
+              erreurEmail: "l'email est invalide",
+            });
+          }
+          if( (!telRegex.test(this.state.tel)) ){
+            e.preventDefault();
+            this.setState({ 
+              erreurTel: "le numéro est invalide",
+            });
+          }
+          if( (!dateRegex.test(this.state.date)) ){
+            e.preventDefault();
+            this.setState({ 
+              erreurDate: "la date est invalide",
+            });
+          }
+
       }
       else{
-
         e.preventDefault();
-        this.state.validation = "";
         this.props.onSubmit(this.state);
 
         this.setState({
@@ -100,7 +135,6 @@ class Formulaire extends React.Component {
           email: "",
           tel: "",
           date: "",
-          validation: "",
         });
       }
   };
@@ -136,22 +170,27 @@ class Formulaire extends React.Component {
 					    <FormControl margin="normal" required fullWidth>
 						    <InputLabel type="text" htmlFor="nom" >Nom</InputLabel>
 						    <Input id="nom" value={this.state.nom} onChange={e => this.Change(e) } name="nom" />
+                <span className={classes.erreur}>{this.state.erreurNom}</span>
 						</FormControl>
 						<FormControl margin="normal" required fullWidth>
 						    <InputLabel htmlFor="prenom">Prénom</InputLabel>
 						    <Input id="prenom" value={this.state.prenom} onChange={e => this.Change(e) } name="prenom" />
+                <span className={classes.erreur}>{this.state.erreurPrenom}</span>
 						</FormControl>
 						<FormControl margin="normal" required fullWidth>
 						    <InputLabel htmlFor="email">Email</InputLabel>
 						    <Input id="email" value={this.state.email} onChange={e => this.Change(e) } name="email" />
+                <span className={classes.erreur}>{this.state.erreurEmail}</span>
 						</FormControl>
 						<FormControl margin="normal" required fullWidth>
 						    <InputLabel htmlFor="tel">Téléphone</InputLabel>
 						    <Input id="tel" value={this.state.tel} onChange={e => this.Change(e) } name="tel" />
+                <span className={classes.erreur}>{this.state.erreurTel}</span>
 						</FormControl>
 						<FormControl margin="normal" required fullWidth>
 						    <InputLabel htmlFor="date">Date</InputLabel>
 						    <Input id="date" value={this.state.date} onChange={e => this.Change(e) } name="date" />
+                <span className={classes.erreur}>{this.state.erreurDate}</span>
 						</FormControl>
 						<Button
 						    type="submit"
